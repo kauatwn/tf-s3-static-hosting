815eeee1d13a086f1e6f00dba425e224e6de6248
kauatwn <kaualopescontato@gmail.com>
2026-08-04 12:21:07 -0300

    feat: add output for CloudWatch Log Group name in WAF module

--- infra/modules/waf/outputs.tf
+++ infra/modules/waf/outputs.tf
@@ -2,3 +2,8 @@ output "web_acl_arn" {
   description = "The ARN of the WAFv2 Web ACL."
   value       = aws_wafv2_web_acl.main.arn
 }
+
+output "log_group_name" {
+  description = "The CloudWatch Log Group name for WAF logs."
+  value       = aws_cloudwatch_log_group.waf_logs.name
+}
