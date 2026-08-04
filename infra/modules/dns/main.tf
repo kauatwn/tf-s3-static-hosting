6b5c5648fe74498c5944fa7ad31ad6cb12744f97
kauatwn <kaualopescontato@gmail.com>
2026-08-04 15:23:13 -0300

    feat(dns): enhance tagging and descriptions for Route 53 resources

--- infra/modules/dns/main.tf
+++ infra/modules/dns/main.tf
@@ -1,11 +1,25 @@
+locals {
+  zone_id = var.create_zone ? aws_route53_zone.primary[0].zone_id : data.aws_route53_zone.existing[0].zone_id
+  common_tags = {
+    Environment = var.environment
+    ManagedBy   = "Terraform"
+    Project     = "StaticSiteHosting"
+    Component   = "DNS"
+  }
+}
+
 # Amazon Route 53 - Hosted Zone creation (Conditional)
 resource "aws_route53_zone" "primary" {
   count = var.create_zone ? 1 : 0
   name  = var.domain_name
 
-  tags = {
-    Name = "HostedZone-${var.domain_name}"
-  }
+  tags = merge(
+    local.common_tags,
+    var.tags,
+    {
+      Name = "HostedZone-${var.domain_name}"
+    }
+  )
 }
 
 # Look up existing Hosted Zone if 'create_zone' is set to false
@@ -15,11 +29,6 @@ data "aws_route53_zone" "existing" {
   private_zone = false
 }
 
-# Local helper to get the effective zone ID
-locals {
-  zone_id = var.create_zone ? aws_route53_zone.primary[0].zone_id : data.aws_route53_zone.existing[0].zone_id
-}
-
 # Route 53 Apex Record (A Record - IPv4 Alias to CloudFront)
 resource "aws_route53_record" "apex" {
   zone_id = local.zone_id
@@ -27,8 +36,8 @@ resource "aws_route53_record" "apex" {
   type    = "A"
 
   alias {
-    name                   = var.cloudfront_domain_name
-    zone_id                = var.cloudfront_hosted_zone_id
+    name                   = var.cloudfront_config.domain_name
+    zone_id                = var.cloudfront_config.hosted_zone_id
     evaluate_target_health = false
   }
 }
@@ -40,8 +49,8 @@ resource "aws_route53_record" "apex_ipv6" {
   type    = "AAAA"
 
   alias {
-    name                   = var.cloudfront_domain_name
-    zone_id                = var.cloudfront_hosted_zone_id
+    name                   = var.cloudfront_config.domain_name
+    zone_id                = var.cloudfront_config.hosted_zone_id
     evaluate_target_health = false
   }
 }
