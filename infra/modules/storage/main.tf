942363f4e76ae83d86f793f9cfff6fbcd7d7fc49
kauatwn <kaualopescontato@gmail.com>
2026-08-04 12:16:30 -0300

    refactor: remove commented-out bucket policy for CloudFront OAC

--- infra/modules/storage/main.tf
+++ infra/modules/storage/main.tf
@@ -17,33 +17,3 @@ resource "aws_s3_bucket_public_access_block" "static_site_access" {
   ignore_public_acls      = true
   restrict_public_buckets = true
 }
-
-# # Bucket Policy - Least Privilege access for CloudFront Origin Access Control (OAC)
-# # Only applied when a CloudFront Distribution ARN is provided
-# resource "aws_s3_bucket_policy" "allow_cloudfront_oac" {
-#   count  = var.cloudfront_distribution_arn != null ? 1 : 0
-#   bucket = aws_s3_bucket.static_site.id
-
-#   policy = jsonencode({
-#     Version = "2012-10-17"
-#     Statement = [
-#       {
-#         Sid    = "AllowCloudFrontServicePrincipalReadOnly"
-#         Effect = "Allow"
-#         Principal = {
-#           Service = "cloudfront.amazonaws.com"
-#         }
-#         Action   = "s3:GetObject"
-#         Resource = "${aws_s3_bucket.static_site.arn}/*"
-#         Condition = {
-#           StringEquals = {
-#             "AWS:SourceArn" = var.cloudfront_distribution_arn
-#           }
-#         }
-#       }
-#     ]
-#   })
-
-#   # Ensure public access block is applied before attaching the bucket policy
-#   depends_on = [aws_s3_bucket_public_access_block.static_site_access]
-# }
