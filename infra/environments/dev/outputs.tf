9b1f6ed28be76addcdb80e8e9378f171037c20d6
kauatwn <kaualopescontato@gmail.com>
2026-08-16 14:10:18 -0300

    refactor: rename modules for improved clarity and organization

--- infra/environments/dev/outputs.tf
+++ infra/environments/dev/outputs.tf
@@ -1,19 +1,19 @@
 output "s3_bucket_name" {
   description = "The unique name (ID) of the S3 bucket created for the dev environment."
-  value       = module.storage.bucket_id
+  value       = module.s3.bucket_id
 }
 
 output "cloudfront_domain_name" {
   description = "The primary domain name of the CloudFront distribution (CDN)."
-  value       = module.cdn.cloudfront_domain_name
+  value       = module.cloudfront.cloudfront_domain_name
 }
 
 output "route53_name_servers" {
   description = "The list of Route 53 Name Servers assigned to the hosted zone."
-  value       = module.dns.name_servers
+  value       = module.route53.name_servers
 }
 
 output "cloudfront_distribution_id" {
   description = "The ID of the CloudFront distribution (used for CDN cache invalidation)."
-  value       = module.cdn.cloudfront_distribution_id
+  value       = module.cloudfront.cloudfront_distribution_id
 }
