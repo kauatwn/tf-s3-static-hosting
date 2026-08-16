c971c8a1e73c5052304129797d6d3a31f4d443c1
kauatwn <kaualopescontato@gmail.com>
2026-08-16 14:10:18 -0300

    refactor: rename modules for improved clarity and organization

--- infra/environments/dev/main.tf
+++ infra/environments/dev/main.tf
@@ -1,6 +1,6 @@
 # 1. Primary Storage Module - S3 Bucket
-module "storage" {
-  source = "../../modules/storage"
+module "s3" {
+  source = "../../modules/s3"
 
   bucket_name = var.bucket_name
   environment = var.environment
@@ -16,14 +16,14 @@ module "waf" {
 }
 
 # 3. CDN Module - CloudFront Distribution + OAC + WAF Association
-module "cdn" {
-  source = "../../modules/cdn"
+module "cloudfront" {
+  source = "../../modules/cloudfront"
 
   environment = var.environment
   domain_name = var.domain_name
   s3_origin = {
-    domain_name = module.storage.bucket_regional_domain_name
-    bucket_id   = module.storage.bucket_id
+    domain_name = module.s3.bucket_regional_domain_name
+    bucket_id   = module.s3.bucket_id
   }
   create_acm_certificate = false # Set to false for LocalStack dev environment
   enable_waf             = var.enable_waf
@@ -31,32 +31,32 @@ module "cdn" {
 }
 
 # 4. Monitoring Module - CloudWatch Metrics & Alarms
-module "monitoring" {
-  source = "../../modules/monitoring"
+module "cloudwatch" {
+  source = "../../modules/cloudwatch"
 
   environment = var.environment
   targets = {
-    cloudfront_distribution_id = module.cdn.cloudfront_distribution_id
+    cloudfront_distribution_id = module.cloudfront.cloudfront_distribution_id
     web_acl_name               = var.enable_waf ? module.waf[0].web_acl_name : null
   }
 }
 
 # 5. DNS Module - Route 53 Hosted Zone + Records
-module "dns" {
-  source = "../../modules/dns"
+module "route53" {
+  source = "../../modules/route53"
 
   domain_name = var.domain_name
   environment = var.environment
   cloudfront_config = {
-    domain_name    = module.cdn.cloudfront_domain_name
-    hosted_zone_id = module.cdn.cloudfront_hosted_zone_id
+    domain_name    = module.cloudfront.cloudfront_domain_name
+    hosted_zone_id = module.cloudfront.cloudfront_hosted_zone_id
   }
   create_zone = true
 }
 
 # 6. S3 Bucket Policy for CloudFront OAC Access
 resource "aws_s3_bucket_policy" "allow_cloudfront_oac" {
-  bucket = module.storage.bucket_id
+  bucket = module.s3.bucket_id
 
   policy = jsonencode({
     Version = "2012-10-17"
@@ -68,10 +68,10 @@ resource "aws_s3_bucket_policy" "allow_cloudfront_oac" {
           Service = "cloudfront.amazonaws.com"
         }
         Action   = "s3:GetObject"
-        Resource = "${module.storage.bucket_arn}/*"
+        Resource = "${module.s3.bucket_arn}/*"
         Condition = {
           StringEquals = {
-            "AWS:SourceArn" = module.cdn.cloudfront_distribution_arn
+            "AWS:SourceArn" = module.cloudfront.cloudfront_distribution_arn
           }
         }
       }
