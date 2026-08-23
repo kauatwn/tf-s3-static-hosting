919d38530616ac7b7dc621409a7d6605b56d8a18
kauatwn <kaualopescontato@gmail.com>
2026-08-23 18:03:49 -0300

    feat: add common tags for resources and output S3 bucket ARN

--- infra/environments/dev/outputs.tf
+++ infra/environments/dev/outputs.tf
@@ -3,17 +3,22 @@ output "s3_bucket_name" {
   value       = module.s3.bucket_id
 }
 
+output "s3_bucket_arn" {
+  description = "The ARN of the S3 bucket."
+  value       = module.s3.bucket_arn
+}
+
 output "cloudfront_domain_name" {
   description = "The primary domain name of the CloudFront distribution (CDN)."
   value       = module.cloudfront.cloudfront_domain_name
 }
 
-output "route53_name_servers" {
-  description = "The list of Route 53 Name Servers assigned to the hosted zone."
-  value       = module.route53.name_servers
-}
-
 output "cloudfront_distribution_id" {
   description = "The ID of the CloudFront distribution (used for CDN cache invalidation)."
   value       = module.cloudfront.cloudfront_distribution_id
+}
+
+output "route53_name_servers" {
+  description = "The list of Route 53 Name Servers assigned to the hosted zone."
+  value       = module.route53.name_servers
 }
